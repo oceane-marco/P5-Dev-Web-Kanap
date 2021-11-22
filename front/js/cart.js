90f9f3e7594ce6a5c4ba685ab9ef662d7a5b3806
Océane Marco <oceane.marco.18@gmail.com>
2021-11-22 14:21:02 +0100

    add finish cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -11,53 +11,26 @@ if (isCartempty()) {
     .then((res) => res.json())
     .then(function (allProducts) {
       let products = [];
-
       productsInCart.forEach((productInCart) => {
-        let product = allProducts.find((item) => item._id == productInCart.id);
-        console.log(product);
-        product.quantity = Number(productInCart.quantity);
-        product.color = productInCart.color;
 
-        products.push(product);
+        let product = allProducts.find((item) => {
+          return item._id == productInCart.id
+        })
+        
+        let item = {...product};
+
+        item.quantity = Number(productInCart.quantity);
+        item.color = productInCart.color;
+
+        products.push(item);
       });
-      console.log(products);
       displayProduct(products);
-      deleteProduct(products, productsInCart);
-      changeQuantity(products, productsInCart);
+      listenDeleteProduct(products, productsInCart);
+      listenChangeQuantity(products, productsInCart);
       displayTotalQuantity(products);
       displayTotalPrice(products);
-     
     });
- 
 }
-//* formulaire 
-//1 verifier les element mis dans le formulaire
-console.log(document.getElementById("firstName"));
-document.getElementById("firstName").addEventListener("change", function () {
-  let regex = new RegExp("^[A-Z][A-Za-ëïzéèê-]+$");
-  if (regex.test(this.value)) {
-    console.log(regex.test(this.value));
-    return true;
-  } else {
-    document.getElementById("firstNameErrorMsg").innerText = "invalide";
-  }
-});
-document.getElementById("lastName").addEventListener("change", function () {
-  let regex = new RegExp("^[A-Z][A-Za-ëïzéèê-]+$");
-  if (regex.test(this.value)) {
-    console.log(regex.test(this.value));
-    return true;
-  } else {
-    document.getElementById("lastNameErrorMsg").innerText = "invalide";
-  }
-  adr
-});
-
-//2 envoyer le formulaire une fois que toutesles donner sont valide 
-
-
-
-
 // fonction
 function isCartempty() {
   if (productsInCart == null || productsInCart.length == 0) {
@@ -66,18 +39,15 @@ function isCartempty() {
     return false;
   }
 }
-
 function displayemptyCard() {
   document.getElementsByTagName("h1")[0].innerText = "Votre panier est vide";
   document.getElementsByClassName("cart").style.display = "none";
 }
-
 function displayProduct(products) {
   products.forEach((product) => {
     document.getElementById("cart__items").innerHTML += render(product);
   });
 }
-
 function render(product) {
   return `
         <article class="cart__item" id="${product._id}-${product.color}">
@@ -116,26 +86,16 @@ function displayTotalQuantity(products) {
   });
   document.getElementById(`totalQuantity`).innerText = totalQuantity;
 }
-function deleteProduct(products, productsInCart) {
+function listenDeleteProduct(products, productsInCart) {
   products.forEach((product) => {
     document
       .getElementById(`delete-button-${product._id}-${product.color}`)
       .addEventListener("click", function () {
-        let confirm = window.confirm(
-          "Etes vous sûr de vouloir supprimer L'article " +
-            product.name +
-            " de couleur " +
-            product.color +
-            " ?"
-        );
-        if (confirm == true) {
-          let index = productsInCart.findIndex(
-            (element) =>
-              element._id == product._id && element.color == product.color
-          );
+        let confirm = window.confirm("Etes vous sûr de vouloir supprimer L'article " + product.name + " de couleur " + product.color + " ?");
+        if (confirm) {
+          let index = productsInCart.findIndex((element) => element.id == product.id && element.color == product.color);
           console.log(index);
           productsInCart.splice(index, 1);
-          console.log(productsInCart);
           localStorage.setItem("products", JSON.stringify(productsInCart));
           document.getElementById(`${product._id}-${product.color}`).remove();
           displayTotalPrice(products);
@@ -144,116 +104,18 @@ function deleteProduct(products, productsInCart) {
       });
   });
 }
-//! a finir 
-function changeQuantity(products, productsInCart) {
+function listenChangeQuantity(products, productsInCart) {
   products.forEach((product) => {
     document.getElementById(`quantity-${product._id}-${product.color}`).addEventListener("change", function () {
       let quantity = this.value;
-      let filterProduct = productsInCart.filter((productInCart) => productInCart.id == product._id || productInCart.color == product.color);
-  
-      filterProduct[0].quantity = Number(quantity);
-      console.log(filterProduct);
-      localStorage.setItem("products", JSON.stringify(productsInCart));
+      let filterProduct = productsInCart.find((productInCart) => productInCart.id == product._id || productInCart.color == product.color);
 
+      filterProduct.quantity = Number(quantity);
       product.quantity = Number(quantity);
+      
+      localStorage.setItem("products", JSON.stringify(productsInCart));
       displayTotalQuantity(products);
-      displayTotalPrice(products);// ne marche que avec le dernier articles
+      displayTotalPrice(products);
     });
   });
 }
-
-
-// console.log(products);
-// let totalArticles = 0;
-// let totalPrice = 0;
-
-// 1 recuperé les donner dans le local.
-// 2 recuperé les donner de l'api pour chaque produits)
-//
-// if (products == null || products.length == 0) {
-//   document.getElementsByTagName("h1")[0].innerText = "Votre panier est vide";
-//   document.getElementById(`totalPrice`).innerText = totalPrice;
-//   document.getElementById(`totalQuantity`).innerText = totalArticles;
-// } else {
-//   for (i = 0; i < products.length; i++) {
-//     let quantity = Number(products[i].quantity);
-//     let color = products[i].color;
-//     let id = products[i].id;
-//     totalArticles += quantity;
-
-//     fetch(`http://localhost:3000/api/products/${products[i].id}`)
-//       .then((res) => res.json())
-//       .then(function (product) {
-//         totalPrice += Number(product.price) * quantity;
-//         displayProduct(product, quantity, color, totalPrice);
-//         listenForDeletProduct(products, id, color);
-//         listenForQuantity(products, quantity);
-
-//         return product;
-//       });
-
-//   }
-
-// }
-
-// function displayProduct(product, quantity, color ) {
-//   document.getElementById("cart__items").innerHTML += `
-//     <article class="cart__item" id="${product._id}">
-//         <div class="cart__item__img">
-//              <img src=${product.imageUrl} alt=${product.altTxt}>
-//         </div>
-//         <div class="cart__item__content">
-//             <div class="cart__item__content__titlePrice">
-//                 <h2>${product.name}</h2>
-//                 <p>${product.price}€</p>
-//                 <p>${color}</p>
-//             </div>
-//             <div class="cart__item__content__settings">
-//             <div class="cart__item__content__settings__quantity">
-//                 <p>Qté : </p>
-//                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value ="${quantity}">
-//             </div>
-//             <div class="cart__item__content__settings__delete">
-//                 <p class="deleteItem">Supprimer</p>
-//             </div>
-//             </div>
-//          </div>
-//     </article>`;
-//   document.getElementById(`totalPrice`).innerText = totalPrice;
-//   document.getElementById(`totalQuantity`).innerText = totalArticles;
-
-// };
-
-// //!résusir a recuperé les bouton suprimer pour les appeler ...
-// //* mettre dans une fonction appeler dans le then.
-// function listenForDeletProduct(products, id, color) {
-//   let deleteItem = document.getElementsByClassName("deleteItem");
-
-//   for (l = 0; l < deleteItem.length; l++) {
-//     deleteItem[l].addEventListener("click", function () {
-//       let confirm = window.confirm(
-//         "Etes vous sûr de vouloir supprimer cet article ?"
-//       );
-//       if (confirm == true) {
-//         products = products.filter(
-//           (product) => product.id !== id || product.color !== color
-//         );
-//         console.log(products);
-//         localStorage.setItem("products", JSON.stringify(products));
-//         location.reload();
-//       }
-//     });
-//   }
-// }
-
-// function listenForQuantity(products, quantity) {
-//   let productsQuantity = document.getElementsByClassName("itemQuantity");
-
-//   for (l = 0; l < productsQuantity.length; l++) {
-
-//     productsQuantity[l].addEventListener("change", function (productsQuantity) {
-//       let quantityValue = Number(productsQuantity[l].value); //! ne fonctionne pas !!!!!
-//       console.log(quantityValue);
-//     });
-//  }
-// };
